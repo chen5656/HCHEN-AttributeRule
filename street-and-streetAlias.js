e9e6b38807eaa6dfc57d526b0ad6c5845ea2a83b
huajunchen <35971289+chen5656@users.noreply.github.com>
2021-07-30 12:30:55 -0500

    Add prevent duplicate street constraint rule.

--- street-and-streetAlias.js
+++ street-and-streetAlias.js
@@ -1,77 +1,71 @@
+//Calculation
 AddStreetId = {
     Description: 'Get Next Street Id',
     Field: STREETID,
     Editable: false,
-    Expression: AddStreetIdExpression,
-    Triggers: 'Insert'
+    Triggers: 'Insert',
+    Expression: function () {
+        return NextSequenceValue('STREET_ID')
+    }
 }
-AddStreetIdExpression = () => {
-    return NextSequenceValue('STREET_ID')
-}
-
 NewStreet_StreetLabel = {
     Description: 'Fill in street label for new street',
     Field: STREETLABEL,
     Editable: false,
-    Expression: NewStreetGetLabelExpression,
-    Triggers: 'Insert'
-}
-NewStreet_StreetLabel_Expression = () => {
-    var parts = ['STREETPREFIX', 'STREETNAME', 'STREETTYPE', 'STREETSUFFIX'];
-    var streetLabel = '';
-    for (var i in parts) {
-        var key = parts[i];
-        var current = Trim($feature[key]);
+    Triggers: 'Insert',
+    Expression: function () {
+        var parts = ['STREETPREFIX', 'STREETNAME', 'STREETTYPE', 'STREETSUFFIX'];
+        var streetLabel = '';
+        for (var i in parts) {
+            var key = parts[i];
+            var current = Trim($feature[key]);
 
-        if (!IsEmpty(current)) {
-            streetLabel = streetLabel + current + ' ';
+            if (!IsEmpty(current)) {
+                streetLabel = streetLabel + current + ' ';
+            }
         }
+        return streetLabel;
     }
-    return streetLabel;
 }
-
 UpdateStreet = {
     Description: 'If values changed, then update Street Label Field and insert a new row to streetAlias',
     Field: STREETLABEL,
     Editable: false,
-    Expression: UpdateStreetExpression,
-    Triggers: 'Update'
-}
-UpdateStreetExpression = () => {
-    var parts = ['STREETPREFIX', 'STREETNAME', 'STREETTYPE', 'STREETSUFFIX'];
-    var streetLabel = '';
-    var isChanged = false;
-    var streetAliasAttr = {};
+    Triggers: 'Update',
+    Expression: function () {
+        var parts = ['STREETPREFIX', 'STREETNAME', 'STREETTYPE', 'STREETSUFFIX'];
+        var streetLabel = '';
+        var isChanged = false;
+        var streetAliasAttr = {};
 
-    for (var i in parts) {
-        var key = parts[i];
-        var current = Trim($feature[key]);
-        var previous = Trim($originalfeature[key]);
+        for (var i in parts) {
+            var key = parts[i];
+            var current = Trim($feature[key]);
+            var previous = Trim($originalfeature[key]);
 
-        streetAliasAttr[key] = previous;
+            streetAliasAttr[key] = previous;
 
-        if (current != previous && isChanged == false) {
-            isChanged = true;
+            if (current != previous && isChanged == false) {
+                isChanged = true;
+            }
+            if (!IsEmpty(current)) {
+                streetLabel = streetLabel + current + ' ';
+            }
+
         }
-        if (!IsEmpty(current)) {
-            streetLabel = streetLabel + current + ' ';
-        }
 
-    }
-
-    if (isChanged) {
-        streetAliasAttr['STREETLABEL'] = $originalfeature.STREETLABEL;
-        streetAliasAttr['STREETID'] = $feature.STREETID;
-        return {
-            'result': streetLabel,
-            'edit': [
-                {
+        if (isChanged) {
+            streetAliasAttr['STREETLABEL'] = $originalfeature.STREETLABEL;
+            streetAliasAttr['STREETID'] = $feature.STREETID;
+            return {
+                'result': streetLabel,
+                'edit': [{
                     'className': 'GISDEV.STREETALIAS',
                     'adds': [{
                         'attributes': streetAliasAttr
                     }]
-                }
-            ]
+                }]
+            }
         }
     }
 }
@@ -80,7 +74,24 @@ UpdateStreetExpression = () => {
 DeleteStreet_CleanStreetAlias = {
     Description: 'If values changed, then update Street Label Field and insert a new row to streetAlias',
     Field: 'STREETID',
-    Editable:false ,
+    Editable: false,
     Expression: DeleteStreetExpression,
     Triggers: 'Delete'
+}
+
+//Constraint
+preventDup = {
+    Description: 'prevent duplicate key value',
+    Triggers: ['Insert', 'Update'],
+    Expression: function () {
+        var fs = FeatureSetByName($datastore, 'GISDEV.STREET');
+        var key = $feature.STREETLABEL;
+        var result = Filter(fs, 'STREETLABEL=@key');
+        var count = Count(result);
+        if (count > 1) {
+            return false
+        }
+
+        return true;
+    }
 }
--- streetAlias.js
+++ streetAlias.js
@@ -8,3 +8,11 @@ AddStreetAliasId = {
 AddStreetAliasIdExpression = () => {
     return NextSequenceValue('STREET_ALIAS_ID')
 }
+
+
+var fsAlias=  FeatureSetByName($datastore, "GISDEV.STREETALIAS", ["globalid"], false);
+var DeleteList = [];
+var streetid=$originalfeature.STREETID;
+var result = Filter(fsAlias, 'STREETID = ' + streetid);
+
+return Count(result)
